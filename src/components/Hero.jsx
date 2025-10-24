a01802afd0c8b17ec8dfd381d10c2cef88c7fd63
JusGoWithTheFLo-MAC <107014024+JusGoWithTheFLo@users.noreply.github.com>
2025-10-24 14:27:44 -0400

    HOME PAGE UPDATE - added large images for higher-res background. - set hero section height to auto for visual appeal. - commented out background image code as placeholder for future background image change on load.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,39 @@
+import { useEffect, useState } from 'react'
 
 // ASSETS
-import bgImage from '../assets/background/hotel-blue-1.jpg'
+import hotelBlue1Small from '../assets/background/hotel-blue-1-small.jpg'
+import hotelBlue1Large from '../assets/background/hotel-blue-1-large.jpg'
 
 
 const Hero = () => {
+    const [bgImage, setBgImage] = useState(hotelBlue1Large)
 
+    // useEffect(() => {
+    //     const updateBackgroundImage = () => {
+    //         if(window.innerWidth < 600 || window.innerHeight < 390 ) {
+    //             setBgImage(hotelBlue1Small)
+    //         } else {
+    //             setBgImage(hotelBlue1Large)
+    //         }
+    //     }
+
+    //     // Run once on mount
+    //     updateBackgroundImage()
+
+    //     // Update on resize
+    //     window.addEventListener('resize', updateBackgroundImage)
+    //     return () => window.removeEventListener('resize', updateBackgroundImage)
+    // }, [])
 
     return (
         <section 
             id='hero' 
-            style={{ backgroundImage: `url(${bgImage})` }}
-            >
+            style={{ backgroundImage: `url(${bgImage})` }}>
+            <div className='hero-content maxw-1200'>
+                <h1>From Transient Stays to <span className='text-yellow'>Thriving Communities</span></h1>
+                <p>We transform underutilized hotels into vibrant apartment ecosystems, fostering a win-win for residents and investors.</p>
+                <a>Learn How We Do It</a>
+            </div>
             
         </section>
     )
